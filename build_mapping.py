fa317e0df532a7a48c03825017c3e85d16714fa7
Tom Parker <palfrey@tevp.net>
2009-02-25 15:44:52 +0000

    Add the "build mapping" script to autogenerate Twitter->LJ mappings

---
+++ build_mapping.py
@@ -0,0 +1,43 @@
+import twitter
+from ConfigParser import SafeConfigParser, NoOptionError
+from pickle import load,dump
+from urllib2 import urlopen, HTTPError
+from sys import stdout
+
+config = SafeConfigParser()
+config.read("settings.ini")
+
+username = config.get("twitter","username")
+
+pname = "friends-%s.pickle"%username
+try:
+	friends = load(file(pname))
+except (OSError,IOError,EOFError):
+	password = config.get("twitter","password")
+
+	api = twitter.Api(username=username,password=password)
+	friends = [x.screen_name for x in api.GetFriends()]
+	friends = dict(zip(friends, [None for x in friends]))
+	dump(friends,file(pname,"wb"))
+print friends
+
+for f in friends:
+	if friends[f] == None:
+		url = "http://%s.livejournal.com"%f.lower()
+		print "url",url
+		try:
+			page = urlopen(url)
+			print f,200
+			friends[f] = True
+		except HTTPError,e:
+			print f,e.code
+			friends[f] = False
+		dump(friends,file(pname,"wb"))
+	
+	if not config.has_option("mapping",f):
+		if friends[f]:
+			config.set("mapping",f,f)
+		else:
+			config.set("mapping",f,"")
+
+config.write(open("settings.ini","w"))
